9ac1eb55dfbce8e69b8b8a9be17dad83770e2adb
ZhangXinxinna <945703764@qq.com>
2022-05-27 21:52:54 +0800

    infowindow

--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -29,13 +29,14 @@
 </template>
 
 <script>
+
 export default {
   data() {
     return {
       zoom: 6,
       center: [117.037107, 35.71569],
       showIndoorMap: false,
-      mapStyle: "amap://styles/light", //修改地图的背景颜色
+      mapStyle: "amap://styles/light",
       markers: [],
       windows: [],
       window: "",
@@ -97,6 +98,9 @@ export default {
       let markers = [];
       let windows = [];
       let that = this;
+      let infoWindow = new AMap.InfoWindow({
+        content: that.$refs.infoWindow.$el,
+      });
       let pointMarker = that.pointMarker;
       pointMarker.forEach((item, index) => {
         markers.push({
@@ -116,14 +120,15 @@ export default {
         });
         windows.push({
           position: [item.lng, item.lat],
-          content: `
-            <div>停车场名称：${item.stationName}</div>
-            <div>停车场位置：${item.stationAddress}</div>
-            <div>车位总数量：<span style='color: #66A0FF'>${item.parkingSpaces}</span></div>
-            <div>剩余车位数量：<span style='color: #66A0FF'>${item.Space}</span></div>
-            <div>价格：<span v-if="!change" style='color: #66A0FF'>${item.price}</div>
-            <button onclick="changePrice('${index}')">修改价格</button>`,
-          offset: [5, -15], //窗体偏移
+          content: 
+        `
+          <div>停车场名称：${item.stationName}</div>
+          <div>停车场位置：${item.stationAddress}</div>
+          <div>车位总数量：<span style='color: #66A0FF'>${item.parkingSpaces}</span></div>
+          <div>剩余车位数量：<span style='color: #66A0FF'>${item.Space}</span></div>
+          <div>价格：<span v-if="!change" style='color: #66A0FF'>${item.price}</div>
+          <button onclick="changePrice('${index}')">修改价格</button>`,
+          offset: [5, -15],
           visible: false, //初始是否显示
         });
       });
@@ -140,22 +145,24 @@ export default {
             <div>剩余车位数量：<span style='color: #66A0FF'>${this.pointMarker[item].Space}</span></div>
             <div>价格： <input value='${this.pointMarker[item].price}' onkeydown="if(event.keyCode==13) saveVal(value,'${item}')"></input></div>`;
     },
-    saveVal(value,index){
-      this.pointMarker[index].price = value
-      this.window.content=`
+    saveVal(value, index) {
+      this.pointMarker[index].price = value;
+      this.window.content = `
             <div>停车场名称：${this.pointMarker[index].stationName}</div>
             <div>停车场位置：${this.pointMarker[index].stationAddress}</div>
             <div>车位总数量：<span style='color: #66A0FF'>${this.pointMarker[index].parkingSpaces}</span></div>
             <div>剩余车位数量：<span style='color: #66A0FF'>${this.pointMarker[index].Space}</span></div>
             <div>价格：<span style='color: #66A0FF'>${this.pointMarker[index].price}</div>
             <button onclick="changePrice('${index}')">修改价格</button>`;
-    }
+    },
   },
   mounted() {
     setTimeout(() => {
       this.point();
     }, 500);
+    console.log(this)
   },
+
 };
 </script>
 
